0676eb6e0df9f59410a5d558eab213e6dacc1b16
Vianny Cruz <vmcc0001@ce.pucmm.edu.do>
2025-06-24 19:40:15 -0400

    Arreglo imports css

--- src/main/java/com/sistema_de_inventarios_v02/Controllers/MainController.java
+++ src/main/java/com/sistema_de_inventarios_v02/Controllers/MainController.java
@@ -1,4 +1,12 @@
 package com.sistema_de_inventarios_v02.Controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MainController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 }
--- src/main/java/com/sistema_de_inventarios_v02/SistemaDeInventariosV02Application.java
+++ src/main/java/com/sistema_de_inventarios_v02/SistemaDeInventariosV02Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SistemaDeInventariosV02Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SistemaDeInventariosV02Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaDeInventariosV02Application.class, args);
+    }
 
 }

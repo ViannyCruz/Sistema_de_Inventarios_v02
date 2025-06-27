1a442b3a2a255e52a1e31ace22b06febbbea1adf
CarlosAle2002 <camm0001@ce.pucmm.edu.do>
2025-06-27 10:23:11 -0400

    Subiendo Migracion de BDD

---
+++ src/main/resources/db/migration/V1__Initial_schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE products (
+                          id BIGINT NOT NULL AUTO_INCREMENT,
+                          name VARCHAR(100) NOT NULL,
+                          description VARCHAR(500),
+                          category VARCHAR(70) NOT NULL,
+                          price DECIMAL(10,2) NOT NULL,
+                          stock INT NOT NULL,
+                          minimum_stock INT,
+                          PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
